a866668d6427f003e9e6a4742e18c075ddd3b52c
Benz Chong Tze Jun <bctj006@gmail.com>
2024-03-25 19:14:09 +0800

    hehehehaw

--- javascript.js
+++ javascript.js
@@ -1,5 +1,19 @@
 function toggleHamburger() {
     const hamburgerMenu = document.getElementsByClassName("HamburgerMenu")[0];
     hamburgerMenu.classList.toggle("active");
-    console.log("toggled")
+    console.log("toggled");
+}
+
+function toggleSwitchTab() {
+    console.log("switch tab");
+
+    const tutoring = document.getElementById('tutoring');
+    const projects = document.getElementById('projects');
+    tutoring.classList.toggle('active');
+    projects.classList.toggle('active');
+
+    const tutoring_page = document.getElementById('tutoring_page');
+    const projects_page = document.getElementById('projects_page');
+    tutoring_page.classList.toggle('inactive');
+    projects_page.classList.toggle('inactive');
 }
