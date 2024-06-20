bb44e56bebc5d7145c701cad1dabe025ef82d7b3
txuyuan <191519R@student.hci.edu.sg>
2024-06-20 14:08:48 +0800

    feat(programmes, volunteer): make 2 pages mobile-friendly

--- javascript.js
+++ javascript.js
@@ -7,13 +7,13 @@ function toggleHamburger() {
 function toggleSwitchTab() {
     console.log("switch tab");
 
-    const tutoring = document.getElementById('tutoring');
-    const projects = document.getElementById('projects');
-    tutoring.classList.toggle('active');
-    projects.classList.toggle('active');
+    const tutoring = document.getElementById("tutoring");
+    const projects = document.getElementById("projects");
+    tutoring.classList.toggle("active");
+    projects.classList.toggle("active");
 
-    const tutoring_page = document.getElementById('tutoring_page');
-    const projects_page = document.getElementById('projects_page');
-    tutoring_page.classList.toggle('inactive');
-    projects_page.classList.toggle('inactive');
+    const tutoring_page = document.getElementById("tutoring_page");
+    const projects_page = document.getElementById("projects_page");
+    tutoring_page.classList.toggle("inactive");
+    projects_page.classList.toggle("inactive");
 }
